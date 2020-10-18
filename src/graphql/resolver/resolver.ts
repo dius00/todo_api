31675ead6607fab4e04e32e8ceac395dc3ed4efb
dius00 <antonio.derinaldis@outlook.com>
2020-10-18 23:58:33 +0900

    completed queries

--- src/graphql/resolver/resolver.ts
+++ src/graphql/resolver/resolver.ts
@@ -12,14 +12,66 @@ import { Resolver, Query, Mutation, Arg } from "type-graphql";
 @Resolver()
 export class GQLResolver {
 
-  @Query(()=> User)
+  @Query(() => User)
   getUser(@Arg("id") id: string) {
-    return User.findOne({where:{id}});
+    return User.findOne(id);
+  }
+
+  @Query(() => List)
+  getList(@Arg("id") id: string) {
+    return List.findOne(id);
+  }
+  
+  @Query(() => [List])
+  getUserLists(@Arg("id") id: string) {
+    return List.find({where: {
+      list_owner: id
+    }});
+  }
+
+  @Query(() => [ToDo])
+  getToDosinList(@Arg("id") id: string) {
+    return ToDo.find({where: {
+      parent_list: id
+    }});
+  }
+
+  @Query(() => ToDo)
+  getToDo(@Arg("id") id: string) {
+    return ToDo.findOne(id);
+  }
+
+  @Mutation(( )=> String)
+  addUser(@Arg("data") data: CreateUser) {
+    const newUser = User.create(data);
+    User.save(newUser);
+    return `${newUser.displayName} has been created`;
   }
 
 
 }
 // }
+// # Add methods (post)
+// addUser(input: User!) :String!
+// addList(input: ListInput!) :String!
+// addToDo(input: ToDoInput!) :String!
+
+// # Modify methods (patch)
+// modifyList(id: String, input: ListInput) :String!
+// makeListPublic(id: String!) :String!
+
+// modifyToDo(id String, input: ToDoInput!) :String!
+
+// # Delete methods (Delete)
+// deleteUser(id: String!) :String!
+// deleteList(id: String!) :String!
+// deleteToDo(id: String!) :String!
+
+
+
+
+
+
 
 // getUser(id: String!): User
 // getList(id: String!): List
